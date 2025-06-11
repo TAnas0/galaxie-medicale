fdcad81911c4871d8d6b4f4bde161d626de0a7f6
TAnas0 <anastiour@gmail.com>
2025-06-11 19:11:03 +0100

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import icon from 'astro-icon';
+import alpinejs from '@astrojs/alpinejs';
+// import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  integrations: [icon(), alpinejs()],
 });
---
+++ src/data/products.js
@@ -0,0 +1,65 @@
+export default [
+    {
+        "name": "Autoclave Phoenix Blu Plus⁺ 18 L",
+        "category": "hospital_equipment",
+        "image": "./catalog/img/phoenix-blu-18-l/Phoenix-Blu-LCD-18.jpg",
+        "description": "Autoclave de classe B 18 L, rapide et conforme aux normes médicales, pour une stérilisation sûre et efficace en cabinet",
+        "availability": "In Stock"
+    },
+    {
+        "name": "Digital ECG Machine",
+        "category": "diagnostic_equipment",
+        "image": "./catalog/img/medical/ecg_machine.jpg",
+        "description": "A digital 12-lead ECG machine with real-time monitoring and data storage.",
+        "price": 3200,
+        "availability": "Pre-Order"
+    },
+    {
+        "name": "Autoclave Sterilizer 50L",
+        "category": "laboratory_equipment",
+        "image": "./catalog/img/medical/autoclave_sterilizer.jpg",
+        "description": "A high-capacity autoclave sterilizer suitable for hospital and laboratory use.",
+        "price": 1800,
+        "availability": "In Stock"
+    },
+    {
+        "name": "Infusion Pump - Smart Control",
+        "category": "hospital_equipment",
+        "image": "./catalog/img/medical/infusion_pump.jpg",
+        "description": "A precise and reliable infusion pump for controlled medication delivery.",
+        "price": 2800,
+        "availability": "In Stock"
+    },
+    // {
+    //     "name": "Automated Hematology Analyzer",
+    //     "category": "laboratory_equipment",
+    //     "image": "./catalog/img/medical/hematology_analyzer.jpg",
+    //     "description": "Advanced blood analysis equipment for complete blood count (CBC) tests.",
+    //     "price": 9500,
+    //     "availability": "Limited Stock"
+    // },
+    // {
+    //     "name": "LED Surgical Operating Light",
+    //     "category": "surgical_equipment",
+    //     "image": "./catalog/img/medical/surgical_light.jpg",
+    //     "description": "Shadowless LED surgical lights with adjustable intensity for operating rooms.",
+    //     "price": 4300,
+    //     "availability": "Pre-Order"
+    // },
+    // {
+    //     "name": "Patient Monitor - Multiparameter",
+    //     "category": "hospital_equipment",
+    //     "image": "./catalog/img/medical/patient_monitor.jpg",
+    //     "description": "Monitors heart rate, oxygen levels, and blood pressure with real-time alerts.",
+    //     "price": 5200,
+    //     "availability": "In Stock"
+    // },
+    {
+        "name": "Digital X-Ray Machine",
+        "category": "diagnostic_equipment",
+        "image": "./catalog/img/medical/xray_machine.jpg",
+        "description": "A state-of-the-art digital X-ray machine with high-resolution imaging capabilities.",
+        "price": 15000,
+        "availability": "Limited Stock"
+    }
+] 
