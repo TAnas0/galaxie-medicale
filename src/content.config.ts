38aa55f26a83fb2618904af70ce6fafdf459e195
TAnas0 <anastiour@gmail.com>
2025-09-03 21:32:06 +0100

    Update products content collection to use new images

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,29 +1,29 @@
 import { defineCollection, z } from 'astro:content';
 import { glob, file } from 'astro/loaders';
 
-const products = defineCollection({
-    loader: file("src/data/products.json"),
-    schema: ({ image }) => z.object({
-        id: z.number(),
-        name: z.string(),
-        category: z.string(),
-        subcategory: z.string().optional(),
-        // category: z.enum(['A', 'B', 'C']),
-        // image: z.string().optional(),
-        image: image().optional(),
-        images: z.array(image()).optional(),
-        description: z.string().optional(),
-        short_description: z.string().optional(),
-        full_description: z.string().optional(),
-        availability: z.string().optional(),
-        brand: z.object({
-            name: z.string().optional(),
-            description: z.string().optional(),
-        }).optional(),
-        // tags: z.array(z.string()),
-        technical_specs: z.record(z.union([z.string(), z.array(z.string())])).optional(),
-    })
-});
+// const products = defineCollection({
+//     loader: file("src/data/products.json"),
+//     schema: ({ image }) => z.object({
+//         id: z.number(),
+//         name: z.string(),
+//         category: z.string(),
+//         subcategory: z.string().optional(),
+//         // category: z.enum(['A', 'B', 'C']),
+//         // image: z.string().optional(),
+//         image: image().optional(),
+//         images: z.array(image()).optional(),
+//         description: z.string().optional(),
+//         short_description: z.string().optional(),
+//         full_description: z.string().optional(),
+//         availability: z.string().optional(),
+//         brand: z.object({
+//             name: z.string().optional(),
+//             description: z.string().optional(),
+//         }).optional(),
+//         // tags: z.array(z.string()),
+//         technical_specs: z.record(z.union([z.string(), z.array(z.string())])).optional(),
+//     })
+// });
 // const brands = defineCollection({ /* ... */ });
 
 const productsFull = defineCollection({
@@ -78,4 +78,4 @@ const productsFull = defineCollection({
 });
 
 
-export const collections = { products, productsFull };
+export const collections = { productsFull };
